12fda20bc067d4d49ab13bde1e837bc33698e553
amir ranjbar <amirranjbar.dev@gmail.com>
2023-01-07 13:25:21 +0330

    v1.0.12

--- fee.go
+++ fee.go
@@ -2,7 +2,6 @@ package bitcoinWallet
 
 import (
 	"errors"
-	"fmt"
 	"github.com/btcsuite/btcd/btcutil"
 	"github.com/btcsuite/btcd/chaincfg"
 	"github.com/btcsuite/btcd/chaincfg/chainhash"
@@ -61,9 +60,6 @@ func estimateTransactionFee(chain *chaincfg.Params, fromAddress string, toAddres
 	transaction.AddTxOut(wire.NewTxOut(amount, toAddressByte))
 
 	transactionSizeInByte := transaction.SerializeSize() + 110
-
-	fmt.Println("transactionSizeInByte")
-	fmt.Println(transactionSizeInByte)
 
 	return int64(transactionSizeInByte * res.EstimatedFees.Slow), nil
 }
