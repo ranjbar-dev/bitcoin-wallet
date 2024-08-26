936509e675b6bde996ed7f963d080f900d09b7b9
Emad Amiripour <emadamiripour307@gmail.com>
2024-08-26 16:27:46 +0330

    Update blockdaemon.go

--- explorer/blockdaemon.go
+++ explorer/blockdaemon.go
@@ -59,12 +59,52 @@ func (e BlockdaemonExplorer) GetAddressBalance(address string) (int, error) {
 
 func (e BlockdaemonExplorer) GetCurrentBlockNumber() (int, error) {
 
-	return 0, nil
+	headers := map[string]string{
+		"X-API-Key": e.ApiKey,
+		"Accept":    "application/json",
+	}
+
+	url := fmt.Sprintf("%s/%s/%s/sync/block_number", e.baseURL, e.Protocol, e.Network)
+
+	client := httpclient.NewHttpclient()
+
+	res, err := client.NewRequest().SetHeaders(headers).Get(url)
+
+	if err != nil {
+		fmt.Println(err)
+		return -1, err
+	}
+
+	val, err := strconv.ParseInt(res.String(), 10, 64)
+
+	if err != nil {
+		fmt.Println(err)
+		return -1, err
+	}
+	return int(val), nil
 }
 
 func (e BlockdaemonExplorer) GetCurrentBlockHash() (string, error) {
 
-	return "", nil
+	headers := map[string]string{
+		"X-API-Key": e.ApiKey,
+		"Accept":    "application/json",
+	}
+
+	url := fmt.Sprintf("%s/%s/%s/sync/block_id", e.baseURL, e.Protocol, e.Network)
+
+	client := httpclient.NewHttpclient()
+
+	res, err := client.NewRequest().SetHeaders(headers).Get(url)
+
+	if err != nil {
+		fmt.Println(err)
+		return "", err
+	}
+
+	fmt.Println(res.String())
+
+	return res.String(), nil
 }
 
 func (e BlockdaemonExplorer) GetBlockByNumber(int) (models.BlockChainBlock, error) {
