13b5dde7f45996b32aae5e896305a143dc20691a
Amir ranjbar <119327352+ranjbar-dev@users.noreply.github.com>
2024-11-28 11:39:00 +0330

    set default config

--- config.go
+++ config.go
@@ -20,7 +20,13 @@ type Config struct {
 
 func init() {
 
-	config = &Config{}
+	config = &Config{
+		Timeout:      10,                                 // http client timeout in second
+		Explorer:     explorer.NewTrezorExplorer(),       // exporer to get blockchain info, check ./exporer folder for other options
+		PriceCrawler: pricecrawler.NewBinanceCraweler(),  // price crawler to get current price data, check ./pricecrawler folder for other options
+		FeeCrawler:   feecrawler.NewBlockstreamCrawler(), // fee crawler to get current fee data, check ./feecrawler folder for other options
+		Chaincfg:     &chaincfg.MainNetParams,            // Bitcoin chain config
+	}
 }
 
 // SetConfig sets the global configuration for the application.
